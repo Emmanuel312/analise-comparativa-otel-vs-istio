74902a177719f21d51526cd5e4ced8a6b9c38a58
Emmanuel312 <emmanuel.nery2019@gmail.com>
2022-09-21 11:34:13 -0300

    Adds istio telemetry api filter

--- run/istio.sh
+++ run/istio.sh
@@ -6,10 +6,10 @@ k config use-context kind-istio
 # install istio and add it in your path 
 istioctl install --set profile=minimal -y
 k label namespace default istio-injection=enabled
-k apply -f ../experiment-02/namespace/sock-shop.yaml
-k apply -f ../experiment-02/namespace/monitoring.yaml
+k apply -f ../experiment-01/namespace/sock-shop.yaml
+k apply -f ../experiment-01/namespace/monitoring.yaml
 k label namespace sock-shop istio-injection=enabled
-k apply -f ../experiment-02/istio/app/app.yaml
+k apply -f ../experiment-01/istio/app/app.yaml
 k port-forward deployments/frontend 8080
 k port-forward deployments/frontend 8079 -n sock-shop
 # prometheus
