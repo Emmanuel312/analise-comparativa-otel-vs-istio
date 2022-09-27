83abc8c738a20f9795ae8d9f6e4eeb304eca2453
Emmanuel312 <emmanuel.nery2019@gmail.com>
2022-09-27 00:10:38 -0300

    Adds kindest image version

--- run/istio.sh
+++ run/istio.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 # cluster + app
-kind create cluster --name istio
+kind create cluster --name istio --image=kindest/node:v1.19.16
 k config use-context kind-istio
 # install istio and add it in your path 
-istioctl install --set profile=minimal -y
+istioctl install --set profile=default --set values.telemetry.v2.enabled=false -y
 k label namespace default istio-injection=enabled
 k apply -f ../experiment-01/namespace/sock-shop.yaml
 k apply -f ../experiment-01/namespace/monitoring.yaml
@@ -13,5 +13,5 @@ k apply -f ../experiment-01/istio/app/app.yaml
 k port-forward deployments/frontend 8080
 k port-forward deployments/frontend 8079 -n sock-shop
 # prometheus
-k apply -f ~/istio-1.14.1/samples/addons/prometheus.yaml
+k apply -f ~/istio-1.15.0/samples/addons/prometheus.yaml
 k port-forward deployment/prometheus 9090 -n istio-system
